79aaf6d57d59b7ba4e90748f9cc42574b817937a
Jacob Howe <howejacob1@gmail.com>
2025-05-14 12:45:41 -0400

    Use WAV file from BACKUPBOY drive for speaker embedding extraction

--- lang_id_models.py
+++ lang_id_models.py
@@ -1,1 +1,6 @@
  # ok now do the same with TitaNet-LID Rep-TDNN whus VoxLingua107 ECAPA-TDNN CommonLanguage ECAPA
+
+import nemo.collections.asr as nemo_asr
+speaker_model = nemo_asr.models.EncDecSpeakerLabelModel.from_pretrained("nvidia/speakerverification_en_titanet_large")
+
+emb = speaker_model.get_embedding("/media/jhowe/BACKUPBOY/fake_wavs/openai_fake_conversation_1747206400811_wvk62e.wav")
