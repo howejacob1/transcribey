a760d2b8836977848846d650c8250e0fa20484eb
Jacob Howe <howejacob1@gmail.com>
2025-05-16 14:45:25 -0400

    Change default MongoDB collection from 'results' to 'vcons'

--- mongo_utils.py
+++ mongo_utils.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 # You can change the URI if you want to use a remote MongoDB or authentication
 MONGO_URI = 'mongodb://localhost:27017/'
 DB_NAME = 'transcribey'
-COLLECTION_NAME = 'results'
+COLLECTION_NAME = 'vcons'
 
 def get_mongo_collection(uri=MONGO_URI, db_name=DB_NAME, collection_name=COLLECTION_NAME):
     client = MongoClient(uri)
