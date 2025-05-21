bc383f68bbaa0fd97add4cfe8e14e1cf5598d091
Temporary User <temp@example.com>
2025-05-21 19:28:47 +0000

    Clear MongoDB collections before running main script

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
+mongo "mongodb://bantaim:tjiSamKAAww@192.168.1.101:27017/vcon-faq" --eval 'db.getSiblingDB("vcon-faq").vcons.deleteMany({}); db.getSiblingDB("vcon-faq").faqs.deleteMany({});'
 source venv/bin/activate
 python main.py 
