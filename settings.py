655b1363b972381003a0dd037fcaf17aa3bf2bb8
Temporary User <temp@example.com>
2025-06-01 22:51:16 +0000

    Remove unused wavs_in_progress_dir and non_en_wavs_in_progress_dir from settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,6 @@
 # Settings for main.py
 
-source_dir = "fake_wavs/"
 dest_dir = "working_memory/raw_wavs_cache/"
-wavs_in_progress_dir = "working_memory/wavs_in_progress_cache/"
-non_en_wavs_in_progress_dir = "working_memory/non_en_wavs_in_progress_cache/"
 lang_detect_threshold = 0.2
 lang_detect_batch_size = 64
 en_transcription_batch_size = 64+32
