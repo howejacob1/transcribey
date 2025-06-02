0e5c7b8b0f8cdfd5d91c821f2c535443c38f9287
Temporary User <temp@example.com>
2025-06-02 22:21:44 +0000

    target fake_Wavs_large also switch to faster model

--- settings.py
+++ settings.py
@@ -5,9 +5,10 @@ lang_detect_threshold = 0.2
 debug = True
 total_vcon_filesize_to_process_bytes = 2 * (1024**3)
 
-transcribe_english_model_name = "nvidia/parakeet-tdt-0.6b-v2"
+#transcribe_english_model_name = "nvidia/parakeet-tdt-0.6b-v2"
+transcribe_english_model_name = "nvidia/parakeet-tdt_ctc-110m"
 transcribe_nonenglish_model_name = "nvidia/canary-1b-flash"
 identify_languages_model_name = "openai/whisper-tiny"
 
 # SFTP connection settings for make_vcons_from_sftp.py
-sftp_url = "sftp://bantaim@192.168.1.103:/home/bantaim/conserver/fake_wavs/"
+sftp_url = "sftp://bantaim@192.168.1.103:/home/bantaim/conserver/fake_wavs_large/"
