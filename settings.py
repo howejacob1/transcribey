bcd4ba0248fece788f7e523396b63a9a877ac0d9
Temporary User <temp@example.com>
2025-06-03 19:56:09 +0000

    changed batching

--- settings.py
+++ settings.py
@@ -9,6 +9,7 @@ total_vcon_filesize_to_process_bytes = 2 * (1024**3)
 transcribe_english_model_name = "nvidia/parakeet-tdt_ctc-110m"
 transcribe_nonenglish_model_name = "nvidia/canary-1b-flash"
 identify_languages_model_name = "openai/whisper-tiny"
+gpu_ram_unusable = 5*1024**3 # 5GB
 
 # SFTP connection settings for make_vcons_from_sftp.py
 sftp_url = "sftp://bantaim@192.168.1.103:/home/bantaim/conserver/fake_wavs_medlarge/"
